203f8cc2dfed5489f1247933e02fb4f280c1426a
vinaythuppari <warangalabbai4@gmail.com>
2025-08-22 15:50:07 +0530

    first commit

---
+++ client/README.md
@@ -0,0 +1,60 @@
+<!-- 1st time ... push
+navagate to Music/PingUp .... not client
+
+git init
+git add .
+git commit -m "Initial commit"
+git remote add origin https://github.com/vinaythuppari/social-media-app.git
+git branch -M main
+git push -u origin main -->
+
+<!-- for updates
+navagate to Music/PingUp .... not client   ... use    cd ..
+git add .
+git commit -m "Updated code"
+git push
+
+git add . && git commit -m "update" && git push -->
+
+
+<!-- npm install moment -->
+<!-- npm install react-hot-toast -->
+
+
+<!-- backend commands 
+npm init -y 
+npm install express cors dotenv mongoose multer
+npm install --save-dev nodemon
+
+npm run server
+
+
+
+inngest
+clerk - authentication google
+mangodb atlas
+-->
+
+
+
+
+
+
+
+
+
+
+
+
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Expanding the ESLint configuration
+
+If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
---
+++ client/src/pages/Feed.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import { dummyPostsData } from '../assets/assets'
+import Loading from '../components/Loading'
+import StoriesBar from '../components/StoriesBar'
+import PostCard from '../components/PostCard'
+import { assets } from '../assets/assets'
+import RecentMessages from '../components/RecentMessages'
+
+const Feed = () => {
+
+  const [feeds, setfeeds] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const fetchFeeds = async () => {
+    setfeeds(dummyPostsData)
+    setLoading(false)
+  }
+
+  useEffect(()=> {
+    fetchFeeds()
+  },[])
+
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5
+    flex items-start justify-center xl:gap-8'>
+      {/* Stories and Post list */}
+
+      <div>
+        <StoriesBar/>
+        <div className='p-4 space-y-6'>
+          {feeds.map((post)=>(
+            <PostCard key={post._id} post={post}/>
+          ))}
+        </div>
+      </div>
+        
+
+      {/* Right sidebar */}
+      <div className='max-xl:hidden sticky top-0'>
+        <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+          <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
+          <img src={assets.sponsored_img} alt="sponsored img" className='w-75 h-50 rounded-md' />
+          <p className='text-slate-600'>Email Marketing</p>
+          <p className='text-slate-400'>Super charge your marketing with a powerful, easy-to-use platform built for results.</p>
+        </div>
+        <RecentMessages/>
+      </div>
+
+    </div>
+  ) : <Loading/>
+}
+
+export default Feed
